d887c7a28f850fb3755c773eecde85ed2abc3c15
Stalin Chiguano <stalin_dany98@hotmail.com>
2024-09-19 21:47:58 -0500

    feat: componente para mostrar productos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ModalDeleteComponent } from './modal-delete/modal-delete.component';
+import { ProductsDetailComponent } from './products-detail/products-detail.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, ModalDeleteComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ModalDeleteComponent, ProductsDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
