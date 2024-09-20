fb10e479aa013cda1a82bf052b1447273b87368f
Stalin Chiguano <stalin_dany98@hotmail.com>
2024-09-19 20:18:19 -0500

    feat: crecion de componentes

---
+++ src/app/modal-delete/modal-delete.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-delete',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-delete.component.html',
+  styleUrl: './modal-delete.component.scss'
+})
+export class ModalDeleteComponent {
+
+}
