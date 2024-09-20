98322e98972c6fdb1d536b0884f180456a140701
Stalin Chiguano <stalin_dany98@hotmail.com>
2024-09-19 23:33:49 -0500

    feat: se crea componente de boton con opciones

---
+++ src/app/floating-button/floating-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-floating-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './floating-button.component.html',
+  styleUrl: './floating-button.component.scss'
+})
+export class FloatingButtonComponent {
+
+}
