765a44217956a82cab9a401364e2dea3e2fe978c
Stalin Chiguano <stalin_dany98@hotmail.com>
2024-09-21 01:09:41 -0500

    feat: delete product

--- src/app/modal-delete/modal-delete.component.ts
+++ src/app/modal-delete/modal-delete.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ProductInterface } from '../interfaces/interfaces';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-modal-delete',
@@ -12,7 +13,15 @@ export class ModalDeleteComponent {
   @Output() closeModal = new EventEmitter<void>();
   @Input() productDelete: ProductInterface = {} as ProductInterface;
 
+  constructor(private productService: ProductService) { }
+
   close() {
     this.closeModal.emit();
+  }
+
+  confirm() {
+    this.productService.deleteProduct(this.productDelete.id).subscribe((data) => {
+      console.log(data);
+    });
   }
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -24,4 +24,8 @@ export class ProductService {
     return this._http.put<any>(`${this.apiUrl}/products/${product.id}`, product);
   }
 
+  deleteProduct(id: string): Observable<any> {
+    return this._http.delete<any>(`${this.apiUrl}/products/${id}`);
+  }
+
 }
