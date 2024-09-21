3fcbe9dec85a80ca3d1022a871e64215a4bb11c4
Stalin Chiguano <stalin_dany98@hotmail.com>
2024-09-21 01:00:04 -0500

    feat: envio del producto para la edicion

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProductService } from '../product.service';
 import { ProductInterface } from '../interfaces/interfaces';
 
@@ -11,13 +10,11 @@ import { ProductInterface } from '../interfaces/interfaces';
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.scss'
 })
-export class RegisterFormComponent {
+export class RegisterFormComponent implements OnInit {
   @Output() closeModal = new EventEmitter<void>();
   @Input() isEdit: boolean = false;
   @Input() editProduct: ProductInterface = {} as ProductInterface;
   bodyProduct: ProductInterface = {} as ProductInterface;
-
-  constructor(private productService: ProductService) { }
 
   formData = {
     id: '',
@@ -27,6 +24,21 @@ export class RegisterFormComponent {
     date_release: '',
     date_revision: '',
   };
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    if (this.isEdit) {
+      this.formData = { ...this.editProduct, date_release: this._formatDate(new Date(this.editProduct.date_release)), date_revision: this._formatDate(new Date(this.editProduct.date_revision)) };
+    }
+  }
+
+  private _formatDate(date: Date): string {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
 
   close() {
     this.closeModal.emit();
